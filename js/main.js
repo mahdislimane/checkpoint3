6b45258bf74fd2174a3d2e5c1adec234d7438e1f
mahdislimane <56697088+mahdislimane@users.noreply.github.com>
2019-11-19 10:55:52 +0100

    commit 2

--- js/main.js
+++ js/main.js
@@ -1,36 +1,52 @@
 var y = "";
+var h = "";
+var n = [];
+var i = 0;
 function button(x) {
+  document.getElementById("result").innerHTML = 0;
   var z = y + x;
   document.getElementById("formul").innerHTML = z;
   y = z;
-  document.getElementById("pos").addEventListener("click", function() {
+  document.getElementById("pos").addEventListener("click", function pos() {
     z += "+";
     y = z;
     document.getElementById("formul").innerHTML = z;
   });
-  document.getElementById("mul").addEventListener("click", function() {
+  document.getElementById("mul").addEventListener("click", function mul() {
     z += "*";
     y = z;
     document.getElementById("formul").innerHTML = z;
   });
-  document.getElementById("division").addEventListener("click", function() {
-    z += "/";
-    y = z;
-    document.getElementById("formul").innerHTML = z;
-  });
-  document.getElementById("neg").addEventListener("click", function() {
+  document
+    .getElementById("division")
+    .addEventListener("click", function division() {
+      z += "/";
+      y = z;
+      document.getElementById("formul").innerHTML = z;
+    });
+  document.getElementById("neg").addEventListener("click", function neg() {
     z += "-";
     y = z;
     document.getElementById("formul").innerHTML = z;
   });
-  document.getElementById("equal").addEventListener("click", function() {
-    document.getElementById("result").innerHTML = eval(z);
+
+  document.getElementById("equal").addEventListener("click", function equal() {
+    document.getElementById("result").innerHTML = "=" + eval(z);
+    document.getElementById("result").style.color = "rgb(14, 68, 3)";
+    m = h + z + "=" + eval(z) + "<br/>";
+    document.getElementById("history").innerHTML = m;
+    n[i] = z + "=" + eval(z) + "<br />";
+    i = i + 1;
+
+    document.getElementById("test").innerHTML = n.join("");
+    h = m;
+    z = "";
+    y = "";
   });
 }
-document.getElementById("raz").addEventListener("click", function() {
-  z = "";
-  y = "";
 
+document.getElementById("raz").addEventListener("click", function raz() {
+  document.getElementById("result").style.color = "black";
   document.getElementById("formul").innerHTML = 0;
   document.getElementById("result").innerHTML = 0;
 });
